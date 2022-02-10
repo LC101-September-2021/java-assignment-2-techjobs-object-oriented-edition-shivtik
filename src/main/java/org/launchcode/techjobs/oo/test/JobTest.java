582cc1a720499dd7fd83232f596afbfbb4482dc3
Shivani <shivani>
2022-02-09 18:32:06 -0600

    Add methods and write first unit test

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,27 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job test_job1, test_job2;
+
+    @Before
+    @Test
+    public void testSettingJobID() {
+        test_job1 = new Job();
+        test_job2 = new Job();
+
+        assertTrue(test_job1.getId() != test_job2.getId());
+    }
+
 
 }
